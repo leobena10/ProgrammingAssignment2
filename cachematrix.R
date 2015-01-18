79704c145bc836da0b0ce01480e9dc9755167f5a
Leandro <leobena08@gmail.com>
2015-01-18 18:03:07 -0300

    Optimized function and comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## If a loop of the same matrix inversion is needed, these two functions
+## will save the first calculation in the cache and use it in the next
+## loops, efectively saving computing time.
 
-## Write a short comment describing this function
+## Takes a matrix to inverse as argument and outputs a list of four needed 
+## functions for the cacheSolve function next.
 
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
-	set <- function(y) {
-		x <<- y
-		m <<- NULL
-	}
 	get <- function() {x}
 	setinv <- function(inv) {m <<- inv}
 	getinv <- function() {m}
-	list(set = set,get = get, setinv = setinv, getinv = getinv)
+	list(get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Takes the list outputted fron makeCacheMatrix, checks cache for inverse 
+## matrix from previous calculation and if not found, calculates it and 
+## outputs it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-	m <- x$getinv()
+	m <- x$getinv() ##Gets the cached matrix
 	if(!is.null(m)) {
 		message("getting cached data")
-		return(m)
+		return(m) ##If a cache matrix exists, it is returned
 	}
-	data <- x$get()
-	m <- solve(data,diag(1,dim(data)[1]), ...)
-	x$setinv(m)
+	matrix <- x$get()
+	m <- solve(matrix,diag(1,dim(matrix)[1]), ...) ## solve(A,b) gives matrix X from b = A X
+	x$setinv(m) ##Sets the inverted matrix to cache
 	return(m)
 }
+## To use, set the cache matrix outside the loop and compute it inside.
+## If not, the cache is reset to NULL each time and it is useless.
